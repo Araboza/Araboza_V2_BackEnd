38f986bd1dc94fd85ed876f09fa6b88edd8de704
시훈 <81404026+sihun96@users.noreply.github.com>
2022-02-05 22:36:21 +0900

    add database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot()
+    ,TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: "Practice",
+      entities: [],
+      synchronize: false,
+      migrationsRun: false
+    }),,AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
