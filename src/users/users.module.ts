fd75df319622fc6a651b72580d7ffe16232b1ed9
시훈 <81404026+sihun96@users.noreply.github.com>
2022-02-09 19:51:34 +0900

    user폴더에 데이터베이스 연결

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { register } from 'src/entities/register.entity';
+import { User } from 'src/entities/user.entity';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([register,User])],
   providers: [UsersService],
 })
 export class UsersModule {}
