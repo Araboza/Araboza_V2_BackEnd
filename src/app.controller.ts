cffb5aab76d7fd5d5b8cf031d5335a38ce013af2
시훈 <81404026+sihun96@users.noreply.github.com>
2022-02-12 22:09:47 +0900

    add

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,8 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-  @Get('S')
-  postS(): string {
-    return this.appService.postS();
-  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,4 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-  postS(): string {
-    return 'post!';
-  }
 }
