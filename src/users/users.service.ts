f70e0b965b78ff97499dd6d9fce89f55d12e4c0e
시훈 <81404026+sihun96@users.noreply.github.com>
2022-02-17 03:07:16 +0900

    Add DTO , createUser

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from 'src/dto/CreateUser.dto';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+  async create(userData: CreateUserDto) {
+    const User = await this.userRepository.create(userData);
+    await this.userRepository.save(User);
+    return User;
+  }
+}
