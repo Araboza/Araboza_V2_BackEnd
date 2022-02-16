87100dd8584f132384e1aac452c3e2727215bd95
시훈 <81404026+sihun96@users.noreply.github.com>
2022-02-17 03:37:27 +0900

    ADD bcrypt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService],
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [AuthService,UsersService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+  public async register(registerData) {
+    const hashedPassword = await bcrypt.hash(registerData.password, 10);
+    try {
+      const createUser = await this.usersService.create({
+        ...registerData,
+        password: hashedPassword,
+      });
+      createUser.password = undefined;
+
+      return createUser;
+    } catch (error) {
+      throw new HttpException(
+        '알 수 없는 오류가 발생하였습니다',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
